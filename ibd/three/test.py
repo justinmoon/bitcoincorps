f92b608a5424a45f5ba71f32565f7e261d52293f
Justin Moen <jamoen7@gmail.com>
2018-08-21 01:40:46 -0500

    Improve test

--- ibd/three/test.py
+++ ibd/three/test.py
@@ -35,21 +35,14 @@ version_message = VersionMessage(
 )
 
 
-def test_version_message_to_bytes():
-    pkt = Packet(command=version_message.command, payload=version_message.to_bytes())
-    print(len(version_message.to_bytes()))
-    pkt_bytes = pkt.to_bytes()
-    pkt2 = Packet.from_socket(FakeSocket(pkt_bytes))
-    version_message_2 = VersionMessage.from_bytes(pkt2.payload)
-    print(len(version_message_2.to_bytes()))
-
-    pkt3 = Packet(command=version_message.command, payload=version_message_2.to_bytes())
-    print("packets 1 & 2 equal: ", pkt.payload == pkt2.payload)
-    print("packets 2 & 3 equal: ", pkt3.payload == pkt2.payload)
-
-    print(version_message)
-    print(version_message_2)
-    assert version_message.__dict__ == version_message_2.__dict__
+def test_version_message_round_trip():
+    version_message_bytes = version_message.to_bytes()
+    packet = Packet(command=version_message.command, payload=version_message_bytes)
+    packet_bytes = packet.to_bytes()
+    sock = FakeSocket(packet_bytes)
+    packet_2 = Packet.from_socket(sock)
+    version_message_2 = VersionMessage.from_bytes(packet_2.payload)
+    assert version_message == version_message_2
 
 
 def test_services_helpers():
